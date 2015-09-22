611ccc7db5c1821fb7aabf5e8e64baf25a388c73
Byron Sommardahl <byron@acklenavenue.com>
2015-09-22 13:30:05 -0500

    Experimenting with layout.

--- src/app.js
+++ src/app.js
@@ -89,86 +89,8 @@ var server = app.listen(3000, function () {
 	var port = server.address().port;
 	var startupMessage = 'Cafe Receipt Printserver ready at http://' + host + ':' + port + ' on ' + moment().format();
 	console.log(startupMessage)
-	printText(startupMessage);
+	printText("__________");
 });
-
-var itemTemplate =  "{0} | {1} | {2}\n";
-
-var footer = 
-	"Gracias por su compra." + "\n" + 
-	"\n" +
-    "La factura es benefició de todos: Exijala!" + "\n" +
-    "No se aceptan cambios ni devoluciones." + "\n" +
-	"\n"+ 
-    "www.CafeElGringo.com";
-
-var itemsHeader = 
-    "----------------------------------------" + "\n"+
-    "Cant.  | Descripcion           |   Valor" + "\n"+
-    "----------------------------------------" + "\n";
-
-var receiptInfo = 
-    "Factura {0}" + "\n"+
-    "Condicion CONTADO" + "\n"+
-	"Fecha/Hora {1}" + "\n"+
-	"Cajero Pamela M - Caja #1" + "\n"	
-    
-var formatReceipt = function(order){	
-	var itemsRendered = getItemsRendered(order.Items).join("");
-	var receiptInfoRendered = getReceiptInfoRendered(order);
-	var totalsRendered = getTotalsRendered(order);
-	var receiptText = header + itemsHeader + itemsRendered + totalsRendered + receiptInfoRendered + footer;
-	return receiptText;
-};
-
-var getReceiptInfoRendered = function(order){
-	var paid = moment(order.Paid);
-	return receiptInfo
-		.format(order._id, 
-			paid.format('MMMM Do YYYY, h:mm:ss a'));
-};
-
-var getItemsRendered = function(items){
-
-	var itemGroups = _.groupBy(items,  function(i){
-		return i.Name;
-	});
-
-	var itemsRendered = _.map(Object.keys(itemGroups), function(itemName){
-		var group = itemGroups[itemName];
-		var sum = 0;
-		if(group.length==1){
-			sum = group[0].Price;
-		}
-		else{
-			sum = _.reduce(group, function(prev, next){
-				return prev.Price + next.Price;
-			});
-		}
-		return itemTemplate.format(
-			leftAlign(group.length, 7), 
-			leftAlign(itemName, 22), 
-			rightAlign(sum, 8));
-	});
-
-	return itemsRendered;
-};
-
-var rightAlign = function(str, len){
-	var stringToAlign = str.toString();
-	if(stringToAlign.length >= len) return stringToAlign;
-	var spaceAmount = len - stringToAlign.length;
-	var spaces = new Array(spaceAmount).join(" ");
-	return spaces + stringToAlign;
-};
-
-var leftAlign = function(str, len){
-	var stringToAlign = str.toString();
-	if(stringToAlign.length >= len) return stringToAlign;
-	var spaceAmount = len - stringToAlign.length;
-	var spaces = new Array(spaceAmount).join(" ");
-	return stringToAlign + spaces;
-};
 
 var getOrderFromRequest = function(data){
 
